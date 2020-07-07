e21dd2d3e56d88909370508a8a9880e3a84160d5
Ritikajain18 <48889754+Ritikajain18@users.noreply.github.com>
2020-07-07 12:22:52 +0530

    Add files via upload

---
+++ Forming a Magic Square.py
@@ -0,0 +1,18 @@
+'''
+Complete the formingMagicSquare function in the editor below. It should return an integer 
+that represents the minimal total cost of converting the input square to a magic square.
+
+formingMagicSquare has the following parameter(s):
+
+s: a 3X3 array of integers'''
+
+s = [[int(i) for i in input().split(' ')] for j in range(3)]
+n = [s[i][j] for i in range(3) for j in range(3)]    
+all_n = [[8,1,6,3,5,7,4,9,2],[6,1,8,7,5,3,2,9,4],[4,9,2,3,5,7,8,1,6],[2,9,4,7,5,3,6,1,8],[8,3,4,1,5,9,6,7,2],[4,3,8,9,5,1,2,7,6],[6,7,2,1,5,9,8,3,4],[2,7,6,9,5,1,4,3,8]]
+
+allsum = []
+for l in all_n:
+    allsum.append(sum([abs(n[i]-l[i]) for i in range(9)]))
+print(min(allsum))
+
+
